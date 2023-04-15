97c2c732d4cb2e3b389c7f5e48043baaf983bce9
Muhammadsaid <muhammadsaid220722@gmail.com>
2023-04-15 15:49:34 +0500

    word repository done

--- lib/repositories/word_repository.dart
+++ lib/repositories/word_repository.dart
@@ -1,3 +1,24 @@
-class WordRepo {
+import 'package:dictionary/models/word_model.dart';
+import 'package:flutter/cupertino.dart';
 
+import '../services/http_services.dart';
+
+class WordRepo {
+  Future<List<WordModel>?> wordRepo() async {
+    final HttpResult response = await ApiRequests().get(slug: 'https://api.dictionaryapi.dev/api/v2/entries/en/WORD',);
+    if(response.isSuccess) {
+      try {
+        debugPrint(response.body);
+        return wordModelFromJson(response.body);
+      }
+      catch(error,s){
+        debugPrint(error.toString());
+        debugPrint(s.toString());
+        return null;
+      }
+    } else {
+      debugPrint(response.body);
+      return null;
+    }
+  }
 }
