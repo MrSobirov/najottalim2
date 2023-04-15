b9258225b5717bf7d712d10b122aa2b09e45e69a
Muhammadsaid <muhammadsaid220722@gmail.com>
2023-04-15 15:45:25 +0500

    word model done

--- lib/models/word_model.dart
+++ lib/models/word_model.dart
@@ -1,3 +1,32 @@
+// To parse this JSON data, do
+//
+//     final wordModel = wordModelFromJson(jsonString);
+
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+List<WordModel> wordModelFromJson(String str) => List<WordModel>.from(json.decode(str).map((x) => WordModel.fromJson(x)));
+
 class WordModel {
+  WordModel({
+    required this.word,
+    required this.audio,
+    required this.definition,
+    required this.synonyms
+
+  });
+
+  final String word;
+  final String audio;
+  final String definition;
+  final List synonyms;
+
+  factory WordModel.fromJson(Map<String, dynamic> json) => WordModel(
+    word: json["word"],
+    audio: json["phonetics"]["audio"],
+    definition: json["meanings"]["definitions"]["definition"],
+    synonyms: json["meanings"]["synonyms"],
+
+  );
 
 }
