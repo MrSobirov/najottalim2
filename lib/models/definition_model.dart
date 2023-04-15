fa6309f9db90f174716d5e104367059485983a87
SobirovI <88648901+SobirovI@users.noreply.github.com>
2023-04-15 14:48:45 +0500

    database done

---
+++ lib/models/definition_model.dart
@@ -0,0 +1,26 @@
+List<DefinitionModel> definitionModelFromJson(List<Map<String, dynamic>> str) => List<DefinitionModel>.from(str.map((x) => DefinitionModel.fromJson(x)));
+
+class DefinitionModel {
+  DefinitionModel({
+    required this.id,
+    required this.word,
+    required this.type,
+    required this.description,
+    required this.idAlphabet,
+  });
+
+  final int id;
+  final String word;
+  final String type;
+  final String description;
+  final int idAlphabet;
+
+  factory DefinitionModel.fromJson(Map<String, dynamic> json) => DefinitionModel(
+    id: json["ID"] ?? 0,
+    word: json["Word"],
+    type: json["Type"] == "()" ? "" : json["Type"],
+    description: json["Description"],
+    idAlphabet: json["ID_ALPHABET"] ?? 0,
+  );
+
+}
---
+++ lib/models/eng_model.dart
@@ -0,0 +1,22 @@
+List<EngUzbModel> engUzbModelFromJson(List<Map<String, dynamic>> str) => List<EngUzbModel>.from(str.map((x) => EngUzbModel.fromJson(x)));
+
+class EngUzbModel {
+  EngUzbModel({
+    required this.id,
+    required this.eng,
+    required this.pron,
+    required this.uzb,
+  });
+
+  final int id;
+  final String eng;
+  final String pron;
+  final String uzb;
+
+  factory EngUzbModel.fromJson(Map<String, dynamic> json) => EngUzbModel(
+    id: json["_id"],
+    eng: json["eng"],
+    pron: json["pron"],
+    uzb: json["uzb"],
+  );
+}
